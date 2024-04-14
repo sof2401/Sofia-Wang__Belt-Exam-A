9f1c7e767fbf571e28eefe9c22d4ef95803b5af6
Yuan Wang <sofiawang2401@gmail.com>
2024-04-14 17:45:48 -0400

    updates

--- exam_functions.py
+++ exam_functions.py
@@ -128,10 +128,73 @@ def make_custom_nlp(
         
     return nlp
 
-import spacy
-nlp = spacy.load("en_core_web_sm", disable=['parser','ner'])
-
-def lemmatize(text):
-    doc = nlp(text)
-    lemmatized_text = [token.lemma_ for token in doc if not token.is_stop and not token.is_punct]
-    return lemmatized_text
+def classification_metrics(y_true, y_pred, label='',
+                           output_dict=False, figsize=(8,4),
+                           normalize='true', cmap='Blues',
+                           colorbar=False,values_format=".2f"):
+    """Modified version of classification metrics function from Intro to Machine Learning.
+    Updates:
+    - Reversed raw counts confusion matrix cmap  (so darker==more).
+    - Added arg for normalized confusion matrix values_format
+    """
+    # Get the classification report
+    report = classification_report(y_true, y_pred)
+    
+    ## Print header and report
+    header = "-"*70
+    print(header, f" Classification Metrics: {label}", header, sep='\n')
+    print(report)
+    
+    ## CONFUSION MATRICES SUBPLOTS
+    fig, axes = plt.subplots(ncols=2, figsize=figsize)
+    
+    # Create a confusion matrix  of raw counts (left subplot)
+    ConfusionMatrixDisplay.from_predictions(y_true, y_pred,
+                                            normalize=None, 
+                                            cmap='gist_gray_r',# Updated cmap
+                                            values_format="d", 
+                                            colorbar=colorbar,
+                                            ax = axes[0]);
+    axes[0].set_title("Raw Counts")
+    
+    # Create a confusion matrix with the data with normalize argument 
+    ConfusionMatrixDisplay.from_predictions(y_true, y_pred,
+                                            normalize=normalize,
+                                            cmap=cmap, 
+                                            values_format=values_format, #New arg
+                                            colorbar=colorbar,
+                                            ax = axes[1]);
+    axes[1].set_title("Normalized Confusion Matrix")
+    
+    # Adjust layout and show figure
+    fig.tight_layout()
+    plt.show()
+    
+    # Return dictionary of classification_report
+    if output_dict==True:
+        report_dict = classification_report(y_true, y_pred, output_dict=True)
+        return report_dict
+    
+def evaluate_classification(model, X_train, y_train, X_test, y_test,
+                         figsize=(6,4), normalize='true', output_dict = False,
+                            cmap_train='Blues', cmap_test="Reds",colorbar=False):
+  # Get predictions for training data
+  y_train_pred = model.predict(X_train)
+  # Call the helper function to obtain regression metrics for training data
+  results_train = classification_metrics(y_train, y_train_pred, #verbose = verbose,
+                                     output_dict=True, figsize=figsize,
+                                         colorbar=colorbar, cmap=cmap_train,
+                                     label='Training Data')
+  print()
+  # Get predictions for test data
+  y_test_pred = model.predict(X_test)
+  # Call the helper function to obtain regression metrics for test data
+  results_test = classification_metrics(y_test, y_test_pred, #verbose = verbose,
+                                  output_dict=True,figsize=figsize,
+                                         colorbar=colorbar, cmap=cmap_test,
+                                    label='Test Data' )
+  if output_dict == True:
+    # Store results in a dataframe if ouput_frame is True
+    results_dict = {'train':results_train,
+                    'test': results_test}
+    return results_dict
